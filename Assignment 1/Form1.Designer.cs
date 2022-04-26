f6d6169585fdb929495ce5c8c97419ac79b6efa9
therecalangian <mfcalangian@up.edu.ph>
2022-04-26 16:16:45 +0800

    First Commit

---
+++ Assignment 1/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace Assignment_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_name = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_name
+            // 
+            this.button_name.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_name.ForeColor = System.Drawing.Color.Black;
+            this.button_name.Location = new System.Drawing.Point(255, 164);
+            this.button_name.Name = "button_name";
+            this.button_name.Size = new System.Drawing.Size(188, 76);
+            this.button_name.TabIndex = 0;
+            this.button_name.Text = "Click me";
+            this.button_name.UseVisualStyleBackColor = true;
+            this.button_name.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(718, 450);
+            this.Controls.Add(this.button_name);
+            this.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_name;
+    }
+}
+
---
+++ Assignment 1/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Assignment_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("I am Mary Joyce F. Calangian","Hello Everyone!");
+        }
+    }
+}
